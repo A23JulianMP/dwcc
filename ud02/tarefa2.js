dc99cb742992d6ce25bea043efdfbc25abbbc1c9
A23JulianMP <A23JulianMP@iessanclemente.net>
2023-10-28 14:38:36 +0200

    ud02 Segunda Tarea control erros

--- ud02/tarefa2.js
+++ ud02/tarefa2.js
@@ -1,7 +1,7 @@
 //Tarefa 2
 const prompt=require("prompt-sync")({sigint:true}); 
-let idade = prompt("Introduza: a súa idade", "50");
-if(idade<=12){
+let idade = prompt("Introduza: a súa idade ", "50");
+if(idade>0 && idade<=12){
     console.log("Neno");
 }else if(idade>12 && idade<=18){
     console.log("Adolescente");
@@ -11,4 +11,6 @@ if(idade<=12){
     console.log("Adulto");
 }else if(idade>64 && idade<=100){
     console.log("Xubilado");
+}else{
+    console.log("Non introduciu un valor correcto");
 }
