3d03e72ccff54d81ab028f0e9316b656177efe7d
A23JulianMP <A23JulianMP@iessanclemente.net>
2023-10-28 11:25:39 +0200

    ud02 Segunda Tarea

---
+++ ud02/tarefa2.js
@@ -0,0 +1,14 @@
+//Tarefa 2
+const prompt=require("prompt-sync")({sigint:true}); 
+let idade = prompt("Introduza: a súa idade", "50");
+if(idade<=12){
+    console.log("Neno");
+}else if(idade>12 && idade<=18){
+    console.log("Adolescente");
+}else if(idade>18 && idade<=30){
+    console.log("Xoven")
+}else if(idade>30 && idade<=64){
+    console.log("Adulto");
+}else if(idade>64 && idade<=100){
+    console.log("Xubilado");
+}
