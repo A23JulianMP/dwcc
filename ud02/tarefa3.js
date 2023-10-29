afe9420100cdac93230be2c7a1d5ac697618b82e
A23JulianMP <A23JulianMP@iessanclemente.net>
2023-10-29 08:53:10 +0100

    ud02 Tarea 3 JS

--- ud02/tarefa3.js
+++ ud02/tarefa3.js
@@ -1,2 +1,15 @@
 //Tarefa 3
 
+let numero = 125;
+let resultado=0;
+resultado=numero>>3;
+console.log(`Divisón entre 8 del número ${numero} el resultado es ${resultado}`);
+numero=40;
+resultado=numero<<2;
+console.log(`Multiplicación por 4 del número ${numero} el resultado es ${resultado}`);
+numero=25
+resultado=numero>>1;
+console.log(`Divisón entre 2 del número ${numero} el resultado es ${resultado}`);
+numero=10;
+resultado=numero<<4;
+console.log(`Multiplicación por 16 del número ${numero} el resultado es ${resultado}`);
